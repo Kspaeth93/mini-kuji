335cb104d0a79dae86ef78b14731148e68436b29
Kevin Spaeth <kevin.spaeth@jda.com>
2017-11-09 15:25:30 -0600

    Added unit test for get_game_board_details_by_space

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -26,11 +26,32 @@ class TestGameBoardTest:
 
     @pytest.fixture
     def game_board_fixture(self):
-        return game_board.GameBoard(3)
+        game_board_fixture = game_board.GameBoard(3)
+        game_board_lists = [None] * 3
+        game_board_lists[0] = ["01", "02", "03"]
+        game_board_lists[1] = ["04", "05", "06"]
+        game_board_lists[2] = ["07", "08", "09"]
+        game_board_details = [None] * 3
+        game_board_details[0] = ["P", "NP", "S"]
+        game_board_details[1] = ["NP", "P", "NP"]
+        game_board_details[2] = ["NP", "NP", "P"]
+        game_board_fixture.game_board_lists = game_board_lists
+        game_board_fixture.game_board_details = game_board_details
+        return game_board_fixture
 
-    def test_get_game_board_data_by_space(self):
-        x = 1 # TODO: Write Test
-        assert x == 0
+    def test_get_game_board_data_by_space(self, game_board_fixture):
+        # Should return None since this is an invalid game board space
+        game_board_space_details = game_board_fixture.get_game_board_details_by_space(0)
+        assert game_board_space_details is None
+        game_board_space_details = game_board_fixture.get_game_board_details_by_space(3)
+        assert game_board_space_details == "S"
+        game_board_space_details = game_board_fixture.get_game_board_details_by_space(6)
+        assert game_board_space_details == "NP"
+        game_board_space_details = game_board_fixture.get_game_board_details_by_space(9)
+        assert game_board_space_details == "P"
+        # Should return None since this is an invalid game board space
+        game_board_space_details = game_board_fixture.get_game_board_details_by_space(10)
+        assert game_board_space_details is None
 
     def test_set_game_board_data_by_space(self):
         x = 1 # TODO: Write Test
